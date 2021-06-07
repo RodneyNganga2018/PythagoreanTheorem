91110599810c3541a298d16cab4a0939b3eec8ae
Rodney Nganga <rnganga2018@gmail.com>
2021-06-07 12:12:34 -0500

    initial commit

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean test = new Pythagorean();
+        double hypotenuse = test.calculateHypotenuse(2, 2);
+        System.out.println("The hypotenuse is " + hypotenuse);
+    }
+}
